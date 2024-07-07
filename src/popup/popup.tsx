72415baefbb731902e2bc236ce686b9ffaf6f91a
Nguyen Tung <tungstun2003@gmail.com>
2024-07-07 09:43:39 +0700

    popup react reactdom render code

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,3 +1,7 @@
 import React from 'react'
-
+import { createRoot } from 'react-dom/client';
 const test = <p>Hello world</p>;
+const rootElement = document.createElement('div')
+document.body.appendChild(rootElement)
+const root = createRoot(rootElement);
+root.render(test);
